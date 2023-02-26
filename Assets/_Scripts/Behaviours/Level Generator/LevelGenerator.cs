abc0a5d6101c8d00c0039ef2d68590925def7ef8
Rickson de Oliveira Viana <rickson2013oliveira@gmail.com>
2023-02-25 21:31:34 -0300

    Revert "Revert "Transition Sections""
    This reverts commit 90099995273286157c2b5e24248415e921a5fc32.

--- Assets/_Scripts/Behaviours/Level Generator/LevelGenerator.cs
+++ Assets/_Scripts/Behaviours/Level Generator/LevelGenerator.cs
@@ -6,12 +6,22 @@ namespace JetMonkey.Generators
 {
     public class LevelGenerator : MonoBehaviour
     {
+        [Header("Sections")]
         public List<GameObject> sectionPrefabs;
+
+        [Space]
+
+        [Header("Transition Sections")]
+        public List<GameObject> transitionPrefabs;
+
+        [Header("Settings")]
         public int numInitialSections = 3;
         public int numExtraSections = 2;
 
         private float levelLength;
-        private float sectionWidth;
+        private float sectionWidth = 0f;
+        private float transitionWidth = 0f;
+
         private List<GameObject> activeSections = new List<GameObject>();
         private Transform playerTransform;
 
@@ -19,10 +29,10 @@ namespace JetMonkey.Generators
         {
             // Calcula o comprimento total do nível
             sectionWidth = sectionPrefabs[0].GetComponent<LevelSection>().GetSectionWidth();
+            transitionWidth = transitionPrefabs[0].GetComponent<LevelSection>().GetSectionWidth();
             levelLength = numInitialSections * sectionWidth;
-            Debug.Log(levelLength);
 
-            for (int i = 0; i < numInitialSections; i++)
+            for (int i = 0; i < numInitialSections; i++) //TODO: Aqui que irá entrar o Início do game
             {
                 // Cria uma nova seção a partir de um prefab aleatório
                 GameObject section = Instantiate(sectionPrefabs[Random.Range(0, sectionPrefabs.Count)], transform);
@@ -68,13 +78,26 @@ namespace JetMonkey.Generators
         {
             // Cria uma nova seção a partir de um prefab aleatório
             GameObject section = Instantiate(sectionPrefabs[Random.Range(0, sectionPrefabs.Count)], transform);
+            GameObject transition = Instantiate(transitionPrefabs[Random.Range(0, transitionPrefabs.Count)], transform);
 
             // Define a posição da seção
-            float sectionStart = activeSections[activeSections.Count - 1].transform.position.x + sectionWidth;
+            float sectionStart = activeSections[activeSections.Count - 1].transform.position.x + transitionWidth;
             section.transform.position = new Vector3(sectionStart, 0f, 0f);
+            Debug.Log(sectionStart);
+
+
+            //Define a posição da Transição
+            float transitionStart = section.transform.position.x + sectionWidth;
+            transition.transform.position = new Vector2(transitionStart, 0f);
+            Debug.Log(transitionStart);
+
 
             // Adiciona a seção à lista de seções ativas
             activeSections.Add(section);
+            activeSections.Add(transition);
+
+            sectionWidth = sectionPrefabs[0].GetComponent<LevelSection>().GetSectionWidth();
+            transitionWidth = transitionPrefabs[0].GetComponent<LevelSection>().GetSectionWidth();
         }
     }
 }
